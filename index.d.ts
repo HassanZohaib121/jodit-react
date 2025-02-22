ac25fe2faaef0dec6585b99ad1c92d3914021df1
Hassan Zohaib <78634222+HassanZohaib121@users.noreply.github.com>
2025-02-23 02:44:31 +0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,28 @@
-import JoditEditor from './build/types/JoditEditor';
-import { Jodit } from './build/types/include.jodit';
+import { Jodit } from 'jodit/esm/index';
+import { ForwardRefExoticComponent, PropsWithoutRef, RefAttributes } from 'react';
+
+type DeepPartial<T> = T extends object
+	? {
+			[P in keyof T]?: DeepPartial<T[P]>;
+	  }
+	: T;
+
+export interface IJoditEditorProps {
+	value: string;
+	name?: string; // Added name property
+
+	className?: string;
+
+	config?: DeepPartial<Jodit['options']>;
+	// eslint-disable-next-line no-unused-vars
+	onChange?: (newValue: string) => void;
+	// eslint-disable-next-line no-unused-vars
+	onBlur?: (newValue: string) => void;
+}
+
+declare const JoditEditor: ForwardRefExoticComponent<
+	PropsWithoutRef<IJoditEditorProps> & RefAttributes<Jodit>
+>;
+
 export default JoditEditor;
 export { Jodit };
