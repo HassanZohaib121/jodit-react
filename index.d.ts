a531d6ee719d42891616a1bf05da180c3eea98be
Hassan Zohaib <78634222+HassanZohaib121@users.noreply.github.com>
2025-04-05 00:30:27 +0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,29 @@
-import JoditEditor from './build/types/JoditEditor';
-import { Jodit } from './build/types/include.jodit';
+// import JoditEditor from './build/types/JoditEditor';
+// import { Jodit } from './build/types/include.jodit';
+// export default JoditEditor;
+// export { Jodit };
+
+import { Jodit } from 'jodit/esm/index';
+import * as React from 'react';
+
+type DeepPartial<T> = T extends object
+	? {
+			[P in keyof T]?: DeepPartial<T[P]>;
+	  }
+	: T;
+
+export interface IJoditEditorProps {
+	value: string;
+	name?: string; 
+	className?: string;
+	config?: DeepPartial<Jodit['options']>;
+	onChange?: (newValue: string) => void;
+	onBlur?: (newValue: string) => void;
+}
+
+declare const JoditEditor: React.ForwardRefExoticComponent<
+	React.PropsWithoutRef<IJoditEditorProps> & React.RefAttributes<Jodit>
+>;
+
 export default JoditEditor;
 export { Jodit };
